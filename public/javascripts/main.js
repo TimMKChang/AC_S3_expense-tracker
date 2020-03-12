a03d61eb7586337a2e7b36f5931b0a894900b5b8
Tim Chang <b00501007@gmail.com>
2020-03-12 22:45:35 +0800

    Add record pie chart by chart.js

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -81,3 +81,61 @@ document.querySelector('#title-amount').addEventListener('click', e => {
   }
 
 })
+
+// chart
+
+createPieChart()
+
+function createPieChart() {
+
+  const category = [
+    'Housing',
+    'Transportation',
+    'Entertainment',
+    'Food',
+    'Others'
+  ]
+
+  const amounts = [0, 0, 0, 0, 0]
+
+  const listGroupChildrenHTML = document.querySelector('.list-group').children
+  for (let i = 1; i < listGroupChildrenHTML.length; i++) {
+    const index = category.indexOf(listGroupChildrenHTML[i].querySelector('.category').dataset.category)
+    amounts[index] += Number(listGroupChildrenHTML[i].querySelector('.record-amount').innerText)
+  }
+
+  const ctx = document.getElementById('recordChart');
+
+  // And for a doughnut chart
+  const myDoughnutChart = new Chart(ctx, {
+    type: 'pie',
+    data: {
+      datasets: [{
+        data: amounts,
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.7)',
+          'rgba(54, 162, 235, 0.7)',
+          'rgba(255, 206, 86, 0.7)',
+          'rgba(255, 159, 64, 0.7)',
+          'rgba(153, 102, 255, 0.7)'
+        ],
+        borderColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(255, 159, 64, 1)',
+          'rgba(153, 102, 255, 1)'
+        ],
+        borderWidth: 1
+      }],
+
+      // These labels appear in the legend and in the tooltips when hovering different arcs
+      labels: category,
+
+    },
+    options: {
+      responsive: false
+    }
+  })
+
+}
