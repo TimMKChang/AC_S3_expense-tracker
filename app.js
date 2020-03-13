5c9caf40000b79a66b5306a5ec2ce1c95e2d15ad
Tim Chang <b00501007@gmail.com>
2020-03-13 18:20:36 +0800

    Modify cookie maxAge from 1 min to 1 day

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(session({
   resave: false,
   saveUninitialized: true,
   store: new MongoStore({ url: process.env.MONGODB_URI || 'mongodb://localhost/sessiondb' }),
-  cookie: { maxAge: 1000 * 60 }
+  cookie: { maxAge: 1000 * 60 * 60 * 24 } // one day
 }))
 
 const passport = require('passport')
